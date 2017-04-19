7f707cfe500c409d4bf77ca6494f09360fe00ff0
Ybrin <koray@koska.at>
2017-04-19 13:03:57 +0200

    Add resource tests

--- test/chatbot_helper/resources_test.rb
+++ test/chatbot_helper/resources_test.rb
@@ -7,10 +7,21 @@ class ChatbotHelper::ResourcesTest < Minitest::Test
     @fixtures = Fixtures::Resources
   end
 
-  def test_message
+  def test_valid_message
     m = ChatbotHelper::Telegram::Message.new(string: @fixtures.valid_message)
 
     assert_equal 4, m.message_id
-    assert_equal 1492106508, m.date
+    assert_equal 1_492_106_508, m.date
+  end
+
+  def test_invalid_message
+    assert_raises(ChatbotHelper::Exceptions::InvalidResource) do
+      ChatbotHelper::Telegram::Message.new(string: @fixtures.invalid_message)
+    end
+
+    assert_raises(ChatbotHelper::Exceptions::InvalidResource) do
+      ChatbotHelper::Telegram::Message.new(string:
+                                           @fixtures.inner_invalid_message)
+    end
   end
 end
--- test/fixtures/resources.rb
+++ test/fixtures/resources.rb
@@ -1,10 +1,16 @@
 module Fixtures
   module Resources
-    # The valid json and its result
+    # A valid message
     @valid_message = '{"message_id":4,"from":{"id":83441420,"first_name":"Koray","username":"Ybrin"},"chat":{"id":83441420,"first_name":"Koray","username":"Ybrin","type":"private"},"date":1492106508,"reply_to_message":{"message_id":2,"from":{"id":83441420,"first_name":"Koray","username":"Ybrin"},"chat":{"id":83441420,"first_name":"Koray","username":"Ybrin","type":"private"},"date":1492106189,"voice":{"duration":2,"mime_type":"audio/ogg","file_id":"AwADBAADagAD-Yh4Uzp0FgPcltErAg","file_size":6383}},"text":"/convertspeech","entities":[{"type":"bot_command","offset":0,"length":14}]}'
 
+    # Invalid message: message_id missing
+    @invalid_message = '{"from":{"id":83441420,"first_name":"Koray","username":"Ybrin"},"chat":{"id":83441420,"first_name":"Koray","username":"Ybrin","type":"private"},"date":1492106508,"reply_to_message":{"message_id":2,"from":{"id":83441420,"first_name":"Koray","username":"Ybrin"},"chat":{"id":83441420,"first_name":"Koray","username":"Ybrin","type":"private"},"date":1492106189,"voice":{"duration":2,"mime_type":"audio/ogg","file_id":"AwADBAADagAD-Yh4Uzp0FgPcltErAg","file_size":6383}},"text":"/convertspeech","entities":[{"type":"bot_command","offset":0,"length":14}]}'
+
+    # Inner invalid message: required object chat: id missing
+    @inner_invalid_message = '{"message_id":4,"from":{"id":83441420,"first_name":"Koray","username":"Ybrin"},"chat":{"first_name":"Koray","username":"Ybrin","type":"private"},"date":1492106508,"reply_to_message":{"message_id":2,"from":{"id":83441420,"first_name":"Koray","username":"Ybrin"},"chat":{"id":83441420,"first_name":"Koray","username":"Ybrin","type":"private"},"date":1492106189,"voice":{"duration":2,"mime_type":"audio/ogg","file_id":"AwADBAADagAD-Yh4Uzp0FgPcltErAg","file_size":6383}},"text":"/convertspeech","entities":[{"type":"bot_command","offset":0,"length":14}]}'
+
     class << self
-      attr_reader :valid_message
+      attr_reader :valid_message, :invalid_message, :inner_invalid_message
     end
   end
 end
